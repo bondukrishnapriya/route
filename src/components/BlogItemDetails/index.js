cfd038d10c50735846f64245e5b38203251e58ba
bondukrishnapriya <priya.kanchi7755@gmail.com>
2024-04-15 11:05:04 +0000

    commit message

---
+++ src/components/BlogItemDetails/index.js
@@ -0,0 +1,68 @@
+// Write your JS code here
+import {Component} from 'react'
+
+import Loader from 'react-loader-spinner'
+import 'react-loader-spinner/dist/loader/css/react-spinner-loader.css'
+
+class BlogItemDetails extends Component {
+  state = {
+    isLoading: true,
+    blogItemDetails: {},
+  }
+
+  componentDidMount = () => {
+    this.getBlogDetails()
+  }
+
+  getBlogDetails = async () => {
+    const {match} = this.props
+    const {params} = match
+    const {id} = params
+    const response = await fetch(`https://apis.ccbp.in/blogs/${id}`)
+    const data = await response.json()
+    const updateData = {
+      title: data.title,
+      imageUrl: data.image_url,
+      content: data.content,
+      avatarUrl: data.avatar_url,
+      author: data.author,
+    }
+
+    this.setState({
+      blogItemDetails: updateData,
+      isLoading: false,
+    })
+  }
+
+  renderBlogItemDetails = () => {
+    const {blogItemDetails} = this.state
+    const {title, imageUrl, content, avatarUrl, author} = blogItemDetails
+    return (
+      <div className="blog-info">
+        <h1 className="blog-details-title">{title}</h1>
+        <div className="author-details">
+          <img className="athor-pic" src={avatarUrl} alt={author} />
+          <p className="details-author-name">{author}</p>
+        </div>
+        <img className="blog-image" src={imageUrl} alt={title} />
+        <p className="blog-content">{content}</p>
+      </div>
+    )
+  }
+
+  render() {
+    const {isLoading} = this.state
+    return (
+      <div className="blog-container">
+        {isLoading ? (
+          <div data-testid="loader">
+            <Loader type="TailSpin" color="#00bfff" height={50} width={50} />
+          </div>
+        ) : (
+          this.renderBlogItemDetails()
+        )}
+      </div>
+    )
+  }
+}
+export default BlogItemDetails
